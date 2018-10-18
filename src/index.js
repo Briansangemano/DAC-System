ac5ea1d580eec30ddd683ca01912b316d544ce8e
Brian Sangermano <briansangermano@gmail.com>
2018-10-18 20:26:06 -0300

    Modification CSS MainMenu and Login

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import Login from './Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Login />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
