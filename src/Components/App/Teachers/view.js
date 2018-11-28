6cfb80dc139ddebe37bdea3e7d7b12e281d64c44
Lucas Malvasia <lucas.malvasia@gmail.com>
2018-11-28 16:04:50 -0300

    get teacher list from back and show

--- src/Components/App/Teachers/view.js
+++ src/Components/App/Teachers/view.js
@@ -5,10 +5,31 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Teachers extends Component {
+
+  componentWillMount() {
+    this.props.getTeachers();
+  }
+
+  getTeacherList = () => {
+    const { teachers } = this.props;
+    return teachers.map(item => (
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+        <tr>
+          <td> { item.id } </td>
+          <td> { item.name } </td>
+        </tr>
+      </table>
+    ))
+  }
   render() {
     return (
       <div className="Teachers">
         <h1>Teachers Page</h1>
+        { this.getTeacherList() }
       </div>
     );
   }
